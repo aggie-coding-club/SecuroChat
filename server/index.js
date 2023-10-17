ba6c3e88df4b2cfab29b57811eeaa35ea35435e3
oliverkstalker <oliver.stalker@outlook.com>
2023-10-17 11:53:09 -0500

    transistioned server from http to websocket

--- server/index.js
+++ server/index.js
@@ -1,38 +1,39 @@
 // server/index.js
 const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
 const dotenv = require('dotenv');
-
-const userRoutes = require('./routes/userRoutes');
-const messageRoutes = require('./routes/messageRoutes');
+const http = require('http');
+const socketIo = require('socket.io');
 
 dotenv.config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
-app.use(cors()); // Handle Cross-Origin Resource Sharing (configure as needed)
-app.use(helmet()); // Set security headers
-app.use(express.json()); // Parses JSON request body
-app.use(express.urlencoded({ extended: true })); // Parses x-www-form-urlencoded
+// Create an HTTP server instance for the express app
+const server = http.createServer(app);
 
-// Routes
-app.use('/api/users', userRoutes);
-app.use('/api/messages', messageRoutes);
-
-// Error handling
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+// Initialize socket.io with the HTTP server instance
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
 });
 
-// Start server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    // Handle custom events or messages here
+    socket.on('message', (data) => {
+        console.log(data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
 });
 
-app.get('/', (req, res) => {
-  res.send('Welcome to SecuroChat!');
+// Start the WebSocket server
+server.listen(PORT, () => {
+    console.log(`WebSocket Server is running on port ${PORT}`);
 });
