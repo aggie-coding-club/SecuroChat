a6f445b9d4a4ef999355512c51b66f824a5570ad
enagelc <enagelc@gmail.com>
2023-10-18 15:42:41 -0500

    some text added, boxes missing

--- client/src/screens/PhoneVerificationScreen.js
+++ client/src/screens/PhoneVerificationScreen.js
@@ -1,5 +1,6 @@
-//PhoneVerficiationScreen.js
-//
+// PhoneVerficiationScreen.js
+// This is a custome React Native component that generates the Phone Verification Screen
+// 
 import react from "react";
 import {
   View,
@@ -9,15 +10,44 @@ import {
 } from "react-native";
 
 const PhoneVerificationScreen = () => {
+    const { veriPhone, enterCodeText, rootContainer, pageHeader } = styles;
     return (
-        <SafeAreaView>
+        <SafeAreaView style={rootContainer}>
             <Text>I am verifying your screen</Text>
+            <View style={pageHeader}>
+                <Text style={veriPhone}>Verify your Number</Text>
+                <Text style={enterCodeText}>Enter the 6-digit code we sent to your phone</Text>
+            </View>
+
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-
+    rootContainer: {
+        flex: 1,
+        backgroundColor: "#FFFFFF",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    pageHeader: {
+        marginTop: 50,
+        flex: 3,
+        alignItems: "center",
+    },
+    veriPhone: {
+        color: "#0078D4",
+        fontSize: 30,
+        fontWeight: "bold",
+        paddingTop: 200,
+    },
+    enterCodeText: {
+        //color: "#000000"
+        color: "#1E1E1E",
+        fontSize: 18,
+        fontWeight: "bold",
+        paddingTop: 20,
+        },
 });
 
 export default PhoneVerificationScreen;
