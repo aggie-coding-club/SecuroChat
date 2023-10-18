edf7778283782e19ce044ec45a8134b3e82f364a
Your Name <you@example.com>
2023-10-18 12:53:35 -0500

    resolved issue #3

--- client/App.js
+++ client/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SignupScreen from './src/screens/SignupScreen';
+import CreateAccountScreen from './src/screens/CreateAccountScreen';
 
 
 const App = () => {
