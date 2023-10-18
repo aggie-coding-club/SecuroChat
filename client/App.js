fe48282a8d2f5e29c2d702436cdc2e813bdbcb81
Your Name <you@example.com>
2023-10-18 15:41:21 -0500

    working on react navigation

--- client/App.js
+++ client/App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignupScreen from './src/screens/SignupScreen';
 import CreateAccountScreen from './src/screens/CreateAccountScreen';
 
 
+const Stack = createNativeStackNavigator();
+
 const App = () => {
   return (
-    <SignupScreen></SignupScreen>
+    <NavigationContainer initialRouteName="Home">
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={SignupScreen}/>
+        <Stack.Screen name="SignUp" component={CreateAccountScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
