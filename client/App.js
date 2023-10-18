a6c2bd7df04ba832d82bb3e991be022d57e33e97
enagelc <enagelc@gmail.com>
2023-10-18 15:06:54 -0500

    Screen viewable in expo

--- client/App.js
+++ client/App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SignupScreen from './src/screens/SignupScreen';
 import CreateAccountScreen from './src/screens/CreateAccountScreen';
+import PhoneVerificationScreen from './src/screens/PhoneVerificationScreen';
 
 
 const App = () => {
   return (
-    <SignupScreen></SignupScreen>
+    <PhoneVerificationScreen></PhoneVerificationScreen>
   );
 }
 
--- client/src/screens/PhoneVerificationScreen.js
+++ client/src/screens/PhoneVerificationScreen.js
@@ -1,1 +1,23 @@
-//test
+//PhoneVerficiationScreen.js
+//
+import react from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
+
+const PhoneVerificationScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>I am verifying your screen</Text>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default PhoneVerificationScreen;
