904482c1f7a4c6de291dbbc975b42b9176f8c14b
Your Name <you@example.com>
2023-10-22 23:55:39 -0500

    updating terms/cookies

--- client/src/screens/SignupScreen.js
+++ client/src/screens/SignupScreen.js
@@ -63,7 +63,7 @@ const SignupScreen = ({ navigation }) => {
         <Text style={infoStyle}>
           By signing up, you agree to our{" "}
           <Text style={inlineLink}>Terms of Service</Text> and{" "}
-          <Text style={inlineLink}>Privacy Policy</Text>, including Cookie Use.
+          <Text style={inlineLink}>Privacy Policy</Text>
         </Text>
       </View>
 
