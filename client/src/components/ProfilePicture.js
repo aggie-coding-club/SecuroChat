3ea4294aed30ece0d7bb98603ff80aacfac2b1b5
Your Name <you@example.com>
2023-10-25 12:13:51 -0500

    updated color methods for background and text for profilePicture

--- client/src/components/ProfilePicture.js
+++ client/src/components/ProfilePicture.js
@@ -12,68 +12,32 @@ import { Text, StyleSheet, View } from "react-native";
  * @param {string} props.color - the base color of the profile picture
  */
 const ProfilePicture = (props) => {
-    backgroundColor = props.color == undefined? hexToRgba('#00FF1B', .25) : hexToRgba(props.color, .25);
-    color = props.color == undefined? darkenColor('#00FF1B', 0.5) : darkenColor(props.color, .5);
-    const size = 100;
-    const fontSize = size * .5;
-
-    return (
-        <View style={[styles.container, { width: size, height: size, borderRadius: size, backgroundColor}]}>
-          <Text style={[styles.initials, {fontSize, color}]}>{props.initials}</Text>
-        </View>
-    );
+  const { container, initials } = styles;
+  return (
+    <View style={[ container, { backgroundColor: `${props.color}30` } ]}>
+      <Text style={[initials, { color: props.color }]}>
+        {props.initials}
+      </Text>
+    </View>
+  );
 };
+
+//globals defining size of textBubble and text itself
+const bubbleSize = 60;
+const textSize = bubbleSize * 0.5;
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    width: bubbleSize,
+    height: bubbleSize,
+    borderRadius: bubbleSize,
   },
   initials: {
-    
-    opacity: 1,
+    fontSize: textSize,
+    fontFamily: "RobotoCondensed_400Regular"
   },
 });
-
-
-/**
- * hexToRgba is a function that creates an rgba color given a hex color
- * @param {string} hex - the base color
- * @param {number} opacity - change to specified opactiy
- */
-const hexToRgba = (hex, opacity) => {
-  const hexColor = hex.replace(/^#/, '');
-  const red = parseInt(hexColor.slice(0, 2), 16);
-  const green = parseInt(hexColor.slice(2, 4), 16);
-  const blue = parseInt(hexColor.slice(4, 6), 16);
-  return `rgba(${red}, ${green}, ${blue}, ${opacity})`;
-};
-
-/**
- * darkenColor darkens a certain color, used for darkening text on top of background color
- * @param {string} color - the base color
- * @param {number} factor - amount to darken by
- */
-const darkenColor = (color, factor) => {
-  const hexColor = color.replace(/^#/, '');
-  let red = parseInt(hexColor.slice(0, 2), 16);
-  let green = parseInt(hexColor.slice(2, 4), 16);
-  let blue = parseInt(hexColor.slice(4, 6), 16);
-
-  red = Math.round(red * (1 - factor));
-  green = Math.round(green * (1 - factor));
-  blue = Math.round(blue * (1 - factor));
-
-  const toHex = (color) => {
-    const hex = color.toString(16);
-    return hex.length === 1 ? `0${hex}` : hex;
-  }
-
-  const redHex = toHex(red);
-  const greenHex = toHex(green);
-  const blueHex = toHex(blue);
-
-  return `#${redHex}${greenHex}${blueHex}`;
-};
 
 export default ProfilePicture;
