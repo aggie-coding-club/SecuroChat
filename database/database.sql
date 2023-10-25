56bc2fab108841659ba116a612eeea203508e38c
cshexin <cshexin@outlook.com>
2023-10-25 01:34:53 -0500

    create the message table  and made some minor changes for the previous tables

--- database/database.sql
+++ database/database.sql
@@ -10,22 +10,27 @@ CREATE TABLE users (
 
 
 -- Create the friends table
+-- NOTES: 
+-- 1. changes the databtype of user_id and friend_id from int to uuid due to the incompatible error
+-- 2. Add the constaint to make sure the only unique tuples can be added to database
 CREATE TABLE friends (
     friendship_id INT PRIMARY KEY,
-    user_id INT REFERENCES users(user_id),
-    friend_id INT REFERENCES users(user_id),
-    status VARCHAR(16) NOT NULL
+    user_id UUID REFERENCES users(user_id),
+    friend_id UUID REFERENCES users(user_id),
+    status VARCHAR(16) NOT NULL,
+    UNIQUE(user_id, friend_id)
 );
 
 
 -- Create the conversations table
+-- NOTES: changes the databtype of creator_id from int to uuid due to the incompatible error
 CREATE TABLE conversations (
     conversation_id INT PRIMARY KEY,
     conversation_type VARCHAR(16) NOT NULL,
     conversation_title VARCHAR(32),
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP,
-    creator_id INT REFERENCES users(user_id)
+    creator_id UUID REFERENCES users(user_id)
 );
 
 -- Create the participants table
@@ -35,4 +40,13 @@ CREATE TABLE participants (
     conversation_id INT REFERENCES conversations(conversation_id)
 );
 
--- TODO: Finish implementing table from schema on lucidchart
+-- Create the messages table 
+CREATE TABLE messages (
+    message_id INT PRIMARY KEY,
+    user_id UUID REFERENCES users(user_id),
+    messages_text VARCHAR(255),
+    timestamp TIMESTAMP,
+    conversation_id INT REFERENCES conversations(conversation_id)
+)
+
+
