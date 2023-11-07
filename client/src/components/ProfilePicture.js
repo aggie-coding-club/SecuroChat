4f04b9347e4e56d557d369756a110241436facf3
Your Name <carlosra0345@gmail.com>
2023-11-07 17:51:59 -0600

    began working on individual chat screen

--- client/src/components/ProfilePicture.js
+++ client/src/components/ProfilePicture.js
@@ -8,34 +8,36 @@ import { Text, StyleSheet, View } from "react-native";
 /**
  * ProfilePicture is a component that generates a general profile picture given specified props
  * @param {object} props - The components props
- * @param {string} props.initials - The text content within the profile picture
- * @param {string} props.color - the base color of the profile picture
+ * @property {string} props.initials - The text content within the profile picture
+ * @property {string} props.color - the base color of the profile picture
+ * @property {number} props.bubbleSize - Sets the size of the profile picture bubble
  */
 const ProfilePicture = (props) => {
+  //globals defining size of textBubble and text itself
+  const bubbleSize = props.bubbleSize;
+  const textSize = bubbleSize * 0.5;
+
   const { container, initials } = styles;
+  const bubbleStyle = {
+    width: bubbleSize,
+    height: bubbleSize,
+    borderRadius: bubbleSize,
+  }
   return (
-    <View style={[ container, { backgroundColor: `${props.color}30` } ]}>
-      <Text style={[initials, { color: props.color }]}>
+    <View style={[ container, { backgroundColor: `${props.color}30` }, bubbleStyle ]}>
+      <Text style={[initials, { color: props.color, fontSize: textSize }]}>
         {props.initials}
       </Text>
     </View>
   );
 };
 
-//globals defining size of textBubble and text itself
-const bubbleSize = 60;
-const textSize = bubbleSize * 0.5;
-
 const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
     alignItems: "center",
-    width: bubbleSize,
-    height: bubbleSize,
-    borderRadius: bubbleSize,
   },
   initials: {
-    fontSize: textSize,
     fontFamily: "RobotoCondensed_400Regular"
   },
 });
