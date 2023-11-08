f11a8692569216997b47c613884e971d6f9bf0e0
Your Name <carlosra0345@gmail.com>
2023-11-07 19:05:34 -0600

    finished work on footer send bar and started ChatMessages component

---
+++ client/src/components/ChatMessage.js
@@ -0,0 +1,20 @@
+/* BackButton.js
+*  This is a custom React Native Component that generates a chat message
+*/
+
+/**
+ * Custom React Native component that renders chat message depending on sender
+ * @param {object} props - Object containing props of component
+ * @property {string} props.content - String containing content of message
+ * @property {number} props.senderID - user id of the sender
+ * 
+ */
+const ChatMessage = (props) => {
+    return (
+        <View>
+            <Text>Hi</Text>
+        </View>
+    );
+};
+
+export default ChatMessage;
