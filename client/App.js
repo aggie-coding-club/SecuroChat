b88975a1f92ff43b77fb0cbc612acacfd92e0cca
Your Name <carlosra0345@gmail.com>
2023-11-13 19:03:47 -0600

    began work on homescreen and navigation within home

--- client/App.js
+++ client/App.js
@@ -2,8 +2,11 @@
 // Main React Native  component resonsible for rendering all other components 
 // controls Screen navigation through React Navigation screen stack 
 
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {useFonts, RobotoCondensed_400Regular, RobotoCondensed_700Bold } from '@expo-google-fonts/roboto-condensed';
 import SignupScreen from './src/screens/SignupScreen';
 import CreateAccountScreen from './src/screens/CreateAccountScreen';
@@ -11,9 +14,45 @@ import PhoneVerificationScreen from './src/screens/PhoneVerificationScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
 import ChatScreen from './src/screens/ChatScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import FriendScreen from './src/screens/FriendScreen';
 
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={SignupScreen}/>
+      <Stack.Screen name="SignUp" component={CreateAccountScreen}/>
+      <Stack.Screen name="PhoneVerification" component={PhoneVerificationScreen} />
+      <Stack.Screen name="LogIn" component={LoginScreen}/>
+      <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen}/>
+      <Stack.Screen name="ChatScreen" component={ChatScreen}/>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const MainStack = () => {
+  return (
+    <Tab.Navigator screenOptions={{
+      headerShown: false, 
+      tabBarStyle: {backgroundColor: '#F6F6F6', height: 90,},
+      tabBarShowLabel: false,
+      }
+    }>
+      <Tab.Screen name="HomeScreen" component={HomeScreen} options={{
+        tabBarIcon: ({ focused }) => (<AntDesign name="message1" size={35} color={focused ? '#1E1E1E' : '#A2A2A2'} />)
+      }}/>
+      <Tab.Screen name="FriendScreen" component={FriendScreen} options={{
+        tabBarIcon: ({ focused }) => (<Ionicons name="people-outline" size={35} color={focused ? '#1E1E1E' : '#A2A2A2'} />)
+      }} />
+    </Tab.Navigator>
+  )
+};
+
 
 const App = () => {
   //logic ensuring font has be loaded successfully before rendiering application
@@ -25,18 +64,14 @@ const App = () => {
     return null;
   }
 
+  // detecting whether user is logged in and authorized
+  let isLoggedIn = true;
+
   return (
     <NavigationContainer initialRouteName="Home">
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Home" component={SignupScreen}/>
-        <Stack.Screen name="SignUp" component={CreateAccountScreen}/>
-        <Stack.Screen name="PhoneVerification" component={PhoneVerificationScreen} />
-        <Stack.Screen name="LogIn" component={LoginScreen}/>
-        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen}/>
-        <Stack.Screen name="ChatScreen" component={ChatScreen}/>
-      </Stack.Navigator>
+      {isLoggedIn ? <MainStack /> : <AuthStack />}
     </NavigationContainer>
   );
-}
+};
 
 export default App;
---
+++ client/src/screens/HomeScreen.js
@@ -0,0 +1,65 @@
+/* HomeScreen.js
+ * This is a custom React component generating the createAccountScreen
+ * Imports API as screen is dynamically populated depending on users current conversations
+ * Other external custom componentes used:
+ * {
+ *   ProfilePicture.js
+ * }
+*/
+
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView, StatusBar, TouchableOpacity } from "react-native";
+import ProfilePicture from '../components/ProfilePicture';
+import { Feather } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+
+
+const HomeScreen = () => {
+    const { rootContainer, header, headerIcons, sectionTitle } = styles;
+    return (
+        <SafeAreaView style={rootContainer}>
+            <StatusBar></StatusBar>
+            <View style={header}>
+                <TouchableOpacity>
+                    <ProfilePicture initials="CA" color="#DB1CD3" bubbleSize={40}></ProfilePicture>
+                </TouchableOpacity>
+                <Text style={sectionTitle}>Chats</Text>
+                <View style={headerIcons}>
+                    <TouchableOpacity>
+                        <Feather name="camera" size={28} color="black" />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name="create-outline" size={30} color="black" />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+};
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        backgroundColor: "#FFFFFF",
+    },
+    header: {
+        paddingTop: 15,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+    },
+    sectionTitle: {
+        fontFamily: "RobotoCondensed_400Regular",
+        fontSize: 25,
+        marginLeft: 20,
+    },
+    headerIcons: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+    }
+});
+
+export default HomeScreen;
