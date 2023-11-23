d3a1231f782696ed56a4a45a1af69368b540d5d8
oliverkstalker <oliver.stalker@outlook.com>
2023-11-23 14:10:28 -0600

    routes for read receipts

---
+++ server/src/routes/readReceiptsRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const readReceiptsController = require('../controllers/readReceiptsController');
+const router = express.Router();
+
+// create a new read receipt
+router.post('/', readReceiptsController.createReadReceipt);
+
+// get read receipts for a message
+router.get('/:messageId', readReceiptsController.getReadReceiptsByMessageId);
